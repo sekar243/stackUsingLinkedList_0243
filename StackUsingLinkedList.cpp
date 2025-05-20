e119f2f4b12dac085df66c6ce2c3b25ddf0cd2a9
sekar243 <kinasekin6602@gmail.com>
2025-05-20 10:20:58 +0700

    membuat fungsi peek dan menampilkan isi stack

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -45,4 +45,23 @@ public:
         cout << "Popped Value: " << top->data << endl;
         top = top->next;
     }
+
+    void peek()
+    {
+        if (top == NULL)
+        {
+            cout << "List is Empty:" << endl;
+        }
+        else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }
+        
+    }
 };
